cf0d06dcb95e72e48992d35697b1236abf404d9a
ruX [Ruslan Zaharov] <git@ruX.vc>
2019-02-20 17:13:05 +0000

    Implement GET / endpoint

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,5 +1,12 @@
 import io.javalin.Javalin
 
+val db = KweetDb().apply {
+    add("ruxmind", "my first kweet")
+    add("ruxmind", "this is so fun")
+    add("alice", "wow it's like kookge minus!")
+    add("jack", "looks like an unicorn startup!")
+}
+
 fun main(args: Array<String>) {
     println("Kwitter startup..")
 
@@ -7,6 +14,10 @@ fun main(args: Array<String>) {
 
     server.get("/ping") { ctx ->
         ctx.result("pong!".repeat(10))
+    }
+
+    server.get("/") { ctx ->
+        ctx.json(db.all())
     }
 
     server.start(7777)
